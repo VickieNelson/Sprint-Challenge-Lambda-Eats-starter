004a9da28a9b1614b502b38a8ba58d4cdce02f38
Vickie Nelson <vickie.isabel.nelson@gmail.com>
2020-05-22 10:25:37 -0500

    styled components installed, imports into App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Route } from "react-router-dom";
 const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
     </>
   );
 };
