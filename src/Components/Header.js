1db187d63a59f8b1778e2c9070e53b5058b47570
Vickie Nelson <vickie.isabel.nelson@gmail.com>
2020-05-22 11:14:03 -0500

    header and navlinks placeholders

---
+++ src/Components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function Header(props) {
+  return <></>;
+
+  <header>
+    <nav>
+      <div>placeholder page title </div>
+      <div>
+        <NavLink>placeholder navlink</NavLink>
+      </div>
+    </nav>
+  </header>;
+}
+
+export default Header;
